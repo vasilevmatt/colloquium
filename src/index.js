3fc1cd95aa52b235f2a1950eb46d9f8e6eadcf82
Matt Vasilev <vasilevmatt@niuitmo.ru>
2024-06-06 17:10:10 +0300

    Second swiper added

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import 'swiper/css/pagination';
 import './index.scss'
 
 
-
+// first swiper
 
 let ButtonHidedSlides = document.querySelectorAll('#hidden-slide')
 
@@ -97,6 +97,104 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             }
             showAllButtonIsClicked = !showAllButtonIsClicked;
+
+        });
+
+    }
+})
+
+
+
+
+
+// second swiper
+
+let ButtonHidedServiceSlides = document.querySelectorAll('#hidden-service-slide')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth
+    if (width < 768) {
+        for (let i = 0; i < ButtonHidedServiceSlides.length; i++) {
+            ButtonHidedServiceSlides[i].classList.remove('service-slide--invisible')
+        }
+        new Swiper('.swiper-second__container', {
+            slideClass: 'swiper-second__slide',
+            wrapperClass: 'swiper-second__wrapper',
+            modules: [Pagination],
+            pagination: {
+                el: '.swiper-second__pagination',
+                clickable: true,
+            },
+            grabCursor: true,
+            slideToClickedSlide: true,
+            slidesPerView: 'auto',
+            spaceBetween: 16,
+            freeMode: true,
+
+            a11y: {
+                enabled: true
+            }
+        });
+    } else {
+
+        let serviceList = document.querySelector('.swiper-second__wrapper')
+
+        let mediumDisplayServiceSlides = document.querySelectorAll('#medium-display-service-slide')
+
+
+        let ServiceListShowAllButton = document.querySelector('#service-list-show-all-button')
+        let ServiceListShowAllButtonText = ServiceListShowAllButton.querySelector('.service-list-show-all-button-text')
+        let ServiceListShowAllButtonIsClicked = false
+        let ServiceListShowAllButtonElement = ServiceListShowAllButton.querySelector('.service-list-show-all-button-element')
+
+        ServiceListShowAllButton.classList.remove('visually-hidden')
+        ServiceListShowAllButton.classList.add('service-list-show-all-button')
+
+        if (width < 1120) {
+            for (let i = 0; i < mediumDisplayServiceSlides.length; i++) {
+                mediumDisplayServiceSlides[i].classList.add('service-slide--invisible')
+            }
+        }
+
+
+
+        serviceList.classList.add('service-list')
+
+
+
+        ServiceListShowAllButton.addEventListener('click', function (evt) {
+            evt.preventDefault();
+
+            let defaultText = 'Показать все'
+            let closeText = 'Скрыть'
+
+
+
+
+            if (ServiceListShowAllButtonIsClicked === false) {
+                ServiceListShowAllButtonText.textContent = closeText
+                ServiceListShowAllButtonElement.classList.add('service-list-show-all-button-clicked-element')
+                for (let i = 0; i < ButtonHidedServiceSlides.length; i++) {
+                    ButtonHidedServiceSlides[i].classList.remove('service-slide--invisible')
+                }
+                for (let i = 0; i < mediumDisplayServiceSlides.length; i++) {
+                    mediumDisplayServiceSlides[i].classList.remove('service-slide--invisible')
+                }
+
+
+            } else {
+                ServiceListShowAllButtonText.textContent = defaultText;
+                ServiceListShowAllButtonElement.classList.remove('service-list-show-all-button-clicked-element')
+                for (let i = 0; i < ButtonHidedServiceSlides.length; i++) {
+                    ButtonHidedServiceSlides[i].classList.add('service-slide--invisible')
+                }
+                if (width < 1120) {
+                    for (let i = 0; i < mediumDisplayServiceSlides.length; i++) {
+                        mediumDisplayServiceSlides[i].classList.add('service-slide--invisible')
+                    }
+                }
+            }
+            ServiceListShowAllButtonIsClicked = !ServiceListShowAllButtonIsClicked;
 
         });
 
